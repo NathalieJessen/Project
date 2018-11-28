bb2d5c6673ab3cad0d5b0e09910a54c806cf1c2d
Nathalie Jessen <naje17ab@student.cbs.dk>
2018-11-28 15:08:04 +0100

    Update shoppingcart.js

--- js/shoppingcart.js
+++ js/shoppingcart.js
@@ -11,22 +11,28 @@ class Order{
     }
   }
   
-
+// Calculate the total dish price based on quantity which is defined with the ID"num"
   function calc() 
   {
     var price = document.getElementById("dishprice").innerHTML;
     var noDish = document.getElementById("num").value;
-    var total = parseFloat(price) * noDish
+    var total = parseInt(price) * noDish
     if (!isNaN(total)) {
       document.getElementById("total").innerHTML = total
       
     }
     
   }
+
+
+// Define our variable, which comes from the element specified as 'checkout-btn'
   var buttons = document.getElementsByClassName('checkout-btn')
 
+  // Pushes the order into local storage 
   var orders = JSON.parse(localStorage.getItem("orders"));;
   
+//If there is no order meaning if the order is null, it should push the two below hardcoded orders to local storage
+
   if(orders == null ) {
       orders = [];
       orders.push(new Order ("Poké Bowl", "60", "2", "monday"));
@@ -34,18 +40,17 @@ class Order{
       
       localStorage.setItem("orders", JSON.stringify(orders));
   
-
+//Else it should store the new orders
     } else {
       orders = JSON.parse(localStorage.getItem('orders'));
   }
   console.log(orders);
 
-
+ // Defines that it should get the above defined elements when the user clicks the checkout button. 
   document.getElementById("checkoutbtn").addEventListener("click", function() {
      orders.push(new Order ("Poké Bowl", "60", document.getElementById("num").value, "monday"));
 
      localStorage.setItem("orders", JSON.stringify(orders));
 
 });
-
 
